8513aef66b30b31064a72062d10d387acf36231f
Timothy Hoang <Timothy Hoang>
2017-04-17 21:07:18 -0500

    exercises 4.17.2017

---
+++ 86 JS Stalker Exercise/assets/js/script.js
@@ -0,0 +1,7 @@
+alert("Welcome to the JS Stalker Exercise!");
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+alert("Thank you for participating " + firstName + " " + lastName + " of " + age + " years old.");
+alert("Have a beautiful day!")
---
+++ 88 Age Calculator Exercise/assets/js/script.js
@@ -0,0 +1,6 @@
+alert("Welcome to the Age Calculator! (We also account leap years)");
+var age = prompt("Tell me your age and I will return how many days you've been alive!");
+var daysAlive = age * 365.25;
+
+alert("You've been alive for roughly " + daysAlive + " days!");
+alert("Here's wishing you a wonderful today and a wonderful tomorrow!");
